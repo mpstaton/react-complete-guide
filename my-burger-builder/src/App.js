13b2cbaca6a56c9241a39453839085e75698abec
Michael Staton <mpstaton@gmail.com>
2020-10-05 10:00:37 -0700

    set up a layout wrapper for burger builder

--- my-burger-builder/src/App.js
+++ my-burger-builder/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends Component {
   render() {
     return (
       <div>
-
+      //This calls a layout component that brings in necessary styles
+        <Layout>
+          <p>Test</p>
+        </Layout>
       </div>
     );
   }
