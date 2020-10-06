8d5101f38100e73469492bb0eaa620a531771686
Michael Staton <mpstaton@gmail.com>
2020-10-06 06:33:35 -0700

    8.130 need to compare to working code in next commit

--- my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,17 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  //apparently the below setting of state replaces the need for constructor and super
+  //note the state object is not an array, so to run array functions on it you have to convert it into an array. 
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
+
   render () {
       return (
         <Aux>
