8417a4a21a8d69402e2fb9b3478c33408eeabd72
Michael Staton <mpstaton@gmail.com>
2020-10-05 17:11:03 -0700

    applied BurgerBuilder container concept to Colearn

--- applied-to-colearn/src/App.js
+++ applied-to-colearn/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
+import MainMessages from './containers/Messages/MainMessages.js'
 
 function App() {
   return (
     <div className="App">
       <Layout>
+        <MainMessages/>
       </Layout>
     </div>
   );
---
+++ applied-to-colearn/src/containers/Messages/MainMessages.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class MainMessages extends Component {
+  render () {
+    return(
+      <Aux>
+          <div>Main Message Component</div>
+      </Aux>
+    );
+  }
+}
+
+export default MainMessages;
